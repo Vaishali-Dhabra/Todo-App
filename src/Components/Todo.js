13c2b0412aaa09be63498dfad7ccca4ad30281ec
Vaishali-Dhabra <vaishali.dhabra@catalyst.sh>
2023-03-10 17:41:59 +0530

    second commit

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,59 +1,74 @@
-import { FcCheckmark } from "react-icons/fc";
-import { FcEmptyTrash } from "react-icons/fc";
-function Todo() {
-    return (
-    <div className="Todo">
-        const[allTodos,setTodos]=useState([]);
-        const[newTitle,setNewTitle]=usestate("");
-        const[newDescription,setNewDescription]=usestate("");
+    import { FcCheckmark } from "react-icons/fc";
+    import { FcEmptyTrash } from "react-icons/fc";
+    import { useState } from "react";
+    function Todo() {
+        const [allTodos, setTodos] = useState([]);
+        const [newTitle, setNewTitle] = useState("");
+        const [newDescription, setNewDescription] = useState("");
 
-        const handleAddTodo = ()=>{
-            let newTodoItem = {
-                title:newTitle,
-                Description:newDescription
-            }
-            let updatedTodoArr=[...allTodos];
-            updatedTodoArr.push(newTodoItem);
-            setTodos(updatedTodoArr);
-        }
+        function handleAddTodo() {
+        let newTodoItem = {
+        title: newTitle,
+        Description: newDescription,
+        };
+        let updatedTodoArr = [...allTodos];
+        updatedTodoArr.push(newTodoItem);
+        setTodos(updatedTodoArr);
+    }
+    return (
+        <div className="Todo">
         <h1>My Todo List</h1>
         <div className="todo-wrapper">
             <div className="todo-input">
             <div className="todo-input-item">
                 <label>Title</label>
-                <input type="text" placeholder="Task's Title" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)} />
+                <input
+                type="text"
+                placeholder="Task's Title"
+                value={newTitle}
+                onChange={(e) => setNewTitle(e.target.value)}
+                />
             </div>
-            
+
             <div className="todo-input-item">
                 <label>Description</label>
-                <input type="text" placeholder="Description" value={newDescription} onChange={(e)=>setNewDescription(e.target.value)}  />
+                <input
+                type="text"
+                placeholder="Description"
+                value={newDescription}
+                onChange={(e) => setNewDescription(e.target.value)}
+                />
             </div>
-            
+
             <div className="todo-input-item">
-                <button type="button" onclick={handleAddTodo} className="Add-Button">Add</button>
+                <button
+                type="button"
+                onclick={handleAddTodo}
+                className="Add-Button"
+                >
+                Add
+                </button>
+            </div>
+            </div>
+            <div className="todo-list">
+            {allTodos.map((item, index) => {
+                return (
+                <div className="todo-list-item" key={index}>
+                    <div>
+                    <h3>{item.title}</h3>
+                    <p>{item.Description}</p>
+                    </div>
+                    <div className="icons">
+                    <FcEmptyTrash className="delete-icon" />
+                    <FcCheckmark className="check-icon" />
+                    </div>
+                </div>
+                );
+            })}
             </div>
         </div>
-        <div className="todo-list">
-
-            {allTodos.map((item,index)=>{
-                return(
-                    <div className="todo-list-item" key={index}>
-                        <div>    
-                            <h3>{item.title}</h3>
-                            <p>{item.Description}</p>
-                        </div>
-                        <div className="icons">
-                            <FcEmptyTrash className="delete-icon"/>
-                            <FcCheckmark className="check-icon" />
-                        </div>
-                    </div>
-                    )
-                })
-            }
         </div>
-        </div>
-    </div>
     );
-}
+    }
 
-export default Todo;
+    export default Todo;
